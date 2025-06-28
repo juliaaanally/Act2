1ea9e6ae1be99400a29acc2340b98ccb654171ca
juliaaanally <julianaallysonelardo@gmail.com>
2025-06-29 00:13:13 +0800

    Add project files.

---
+++ Act2/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+using System.Diagnostics;
+using Act2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Act2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RecentlySeen()
+        {
+            return View();
+        }
+
+        public IActionResult PopularPicks()
+        {
+            return View();
+        }
+
+        public IActionResult Lists()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Act2/Models/RecentlySeen.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Act2.Models
+{
+    public class RecentlySeen
+    {
+        public int Id { get; set; }
+        public decimal Value { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+    }
+}
